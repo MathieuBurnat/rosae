0104d7f2a7d18a9775c5ffe23f4b347d96ef41a1
Mathieu.Burnat@cpnv.ch <Mathieu.Burnat@cpnv.ch>
2022-05-25 21:41:46 +0200

    prepare for the increment of retrieve and register component

---
+++ src/components/Client/Register.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1> Register section</h1>
+  </div>
+</template>
--- src/pages/Client.vue
+++ src/pages/Client.vue
@@ -3,43 +3,43 @@
     <div class="grid h-auto card rounded-box place-items-center">
       <h1>Client section {{ count }}</h1>
       <div>
-        <button
-          class="w-40 m-1 btn"
-          @click="openRegister"
-        >
-          Register
-        </button>
+        <button class="w-40 m-1 btn" @click="openRegister">Register</button>
 
-        <button
-          class="w-40 m-1 btn"
-          @click="openRetrieve"
-        >
-          Retrieve
-        </button>
+        <button class="w-40 m-1 btn" @click="openRetrieve">Retrieve</button>
       </div>
     </div>
     <div class="divider"></div>
     <div class="grid h-20 card rounded-box place-items-center">
-      {{ content }} 
+      <Register v-if="displayRegister" />
+      <Retrieve v-if="displayRetrieve" />
     </div>
   </div>
 </template>
 
 <script>
+import Retrieve from "../components/client/Retrieve.vue";
+import Register from "../components/client/Register.vue";
+
 export default {
-  components: {},
+  components: {
+    Register,
+    Retrieve,
+  },
   data() {
     return {
-      content: "",
+      displayRegister: false,
+      displayRetrieve: false,
     };
   },
   methods: {
     openRegister() {
-      this.content = "register";
+      this.displayRetrieve = false;
+      this.displayRegister = true;
     },
     openRetrieve() {
-      this.content = "retrieve";
-    }
+      this.displayRetrieve = true;
+      this.displayRegister = false;
+    },
   },
 };
 </script>
