fdc60f4f2c7590bc0a2aeb4eb528b3461261e793
Mathieu.Burnat@cpnv.ch <Mathieu.Burnat@cpnv.ch>
2022-05-25 12:33:47 +0200

    init files

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script>
+import Home from "./pages/Homepage.vue";
+import About from "./pages/About.vue";
+import NotFound from "./pages/NotFound.vue";
+import Header from "./components/navbar/Navbar.vue";
+
+// Implement a basic router
+const routes = {
+  "/": Home,
+  "/about": About,
+};
+
+export default {
+  components : {
+    Header,
+  },
+
+  data() {
+    return {
+      currentPath: window.location.hash,
+    };
+  },
+  computed: {
+    currentView() {
+      return routes[this.currentPath.slice(1) || "/"] || NotFound;
+    },
+  },
+  mounted() {
+    window.addEventListener("hashchange", () => {
+      this.currentPath = window.location.hash;
+    });
+  },
+};
+</script>
+
+<template>
+  <Header />
+  <component :is="currentView" />
+</template>
---
+++ src/pages/Homepage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1> Homepage </h1>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  components: {
+  },
+  setup() {
+  },
+};
+</script>
