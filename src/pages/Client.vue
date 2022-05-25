2651edfd955d5a109c05d70549a69b33c6f9e26b
Mathieu.Burnat@cpnv.ch <Mathieu.Burnat@cpnv.ch>
2022-05-25 21:25:51 +0200

    create client page

--- src/pages/Client.vue
+++ src/pages/Client.vue
@@ -1,12 +1,45 @@
 <template>
   <div>
-    <h1> Client section </h1>
+    <div class="grid h-auto card rounded-box place-items-center">
+      <h1>Client section {{ count }}</h1>
+      <div>
+        <button
+          class="w-40 m-1 btn"
+          @click="openRegister"
+        >
+          Register
+        </button>
+
+        <button
+          class="w-40 m-1 btn"
+          @click="openRetrieve"
+        >
+          Retrieve
+        </button>
+      </div>
+    </div>
+    <div class="divider"></div>
+    <div class="grid h-20 card rounded-box place-items-center">
+      {{ content }} 
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   components: {},
-  setup() {},
+  data() {
+    return {
+      content: "",
+    };
+  },
+  methods: {
+    openRegister() {
+      this.content = "register";
+    },
+    openRetrieve() {
+      this.content = "retrieve";
+    }
+  },
 };
 </script>
