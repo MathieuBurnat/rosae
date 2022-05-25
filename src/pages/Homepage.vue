dee37e9fe63203abe2bb3e992a8c649b7bfe92fe
Mathieu.Burnat@cpnv.ch <Mathieu.Burnat@cpnv.ch>
2022-05-25 19:53:50 +0200

    upgrade content with flex around

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">Homepage !</h1>
-    <button class="btn btn-info">Info</button>
-    <button class="btn btn-success">Success</button>
-    <button class="btn btn-warning">Warning</button>
-    <button class="btn btn-error">Error</button>
+  <div class="flex flex-col w-full">
+    <h1>Home page !</h1>
+    <div class="grid h-20 card rounded-box place-items-center">content</div>
+    <div class="divider"></div>
+    <div class="grid h-20 card rounded-box place-items-center">content</div>
+    <div class="divider">
+      <button class="btn btn-info">Info</button>
+      <button class="btn btn-success">Success</button>
+      <button class="btn btn-warning">Warning</button>
+      <button class="btn btn-error">Error</button>
+    </div>
   </div>
 </template>
 
