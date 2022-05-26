58652eb4e50bb61705889988feae7b61b2d60e0b
Mathieu.Burnat@cpnv.ch <Mathieu.Burnat@cpnv.ch>
2022-05-26 14:00:49 +0200

    add correct values

--- src/components/Client/Register.vue
+++ src/components/Client/Register.vue
@@ -1,42 +1,64 @@
 <template>
   <div>
     <div class="mb-4">
-      <label class="block mb-2 text-sm font-bold text-gray-700" for="username">
-        Username
+      <div class="mb-4">
+        <label class="block mb-2 text-sm font-bold text-gray-700" for="email">
+          email
+        </label>
+        <input
+          class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+          id="email"
+          type="email"
+          placeholder="email"
+          v-model="email"
+        />
+      </div>
+
+      <label class="block mb-2 text-sm font-bold text-gray-700" for="firstname">
+        firstname
       </label>
       <input
         class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-        id="username"
+        id="firstname"
         type="text"
-        placeholder="Username"
+        placeholder="firstname"
+        v-model="firstname"
       />
     </div>
 
     <div class="mb-4">
-      <label class="block mb-2 text-sm font-bold text-gray-700" for="email">
-        email
+      <label class="block mb-2 text-sm font-bold text-gray-700" for="lastname">
+        lastname
       </label>
       <input
         class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-        id="email"
-        type="email"
-        placeholder="email"
-      />
-    </div>
-
-    <div class="mb-4">
-      <label class="block mb-2 text-sm font-bold text-gray-700" for="data">
-        data
-      </label>
-      <input
-        class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-        id="username"
+        id="lastname"
         type="text"
-        placeholder="Username"
+        placeholder="lastname"
+        v-model="lastname"
       />
     </div>
 
-    <button class="btn btn-success">Save </button>
-
+    <button class="btn btn-success" @click="register">Save</button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      firstname: "",
+      lastname: "",
+    };
+  },
+  methods: {
+    register() {
+      console.log("- Register the clien -");
+      console.log(this.email);
+      console.log(this.firstname);
+      console.log(this.lastname);
+    },
+  },
+};
+</script>
