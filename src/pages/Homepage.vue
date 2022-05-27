d5a2b93c5c9d96db6d61fcd4ba61060f6ec1705b
Mathieu.Burnat@cpnv.ch <Mathieu.Burnat@cpnv.ch>
2022-05-27 19:40:26 +0200

    upgrade the homepage styles

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -1,21 +1,41 @@
 <template>
-  <div class="flex flex-col w-full">
-    <h1>Home page !</h1>
-    <div class="grid h-20 card rounded-box place-items-center">content</div>
-    <div class="divider"></div>
-    <div class="grid h-20 card rounded-box place-items-center">content</div>
-    <div class="divider">
-      <button class="btn btn-info">Info</button>
-      <button class="btn btn-success">Success</button>
-      <button class="btn btn-warning">Warning</button>
-      <button class="btn btn-error">Error</button>
+  <div class="min-h-screen hero bg-base-200">
+    <div class="text-center hero-content">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Welcome to 🚀</h1>
+        <p class="py-6">
+          <span class="font-bold"> Rosae </span> is a website wich allows you to
+          test the
+          <a
+            class="btn-link"
+            href="https://github.com/MathieuBurnat/api-client-bc"
+            >api-client-bc</a
+          >
+          API. You will be able to find out many data of our products while
+          seeing them inside our database and a blockchain.
+        </p>
+        <p class="mb-6">
+          You could also play many roles as a client, provider or seller to
+          execute different interaction.
+        </p>
+        <a href="#/data" class="gap-2 btn btn-primary">
+          Get Started
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="w-6 h-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+            />
+          </svg>
+        </a>
+      </div>
     </div>
   </div>
 </template>
-
-<script>
-export default {
-  components: {},
-  setup() {},
-};
-</script>
