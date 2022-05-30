6f5964fc0254aa36227d4a206e384495d381b805
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2022-05-30 10:49:37 +0200

    Create the seller page
    Create the seller page

---
+++ src/components/Seller/CreateProduct.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1> Create a product </h1>
+  </div>
+</template>
--- src/pages/Seller.vue
+++ src/pages/Seller.vue
@@ -1,12 +1,45 @@
 <template>
   <div>
-    <h1> Seller section </h1>
+    <div class="grid h-auto card rounded-box place-items-center">
+    <h1> Provider section </h1>
+      <div>
+        <button class="w-40 m-1 btn" @click="openProduct">New product</button>
+
+        <button class="w-40 m-1 btn" @click="openOffer">New offer</button>
+      </div>
+    </div>
+    <div class="divider"></div>
+    <div class="grid card rounded-box place-items-center">
+      <Product v-if="displayProduct" />
+      <Offer v-if="displayOffer" />
+    </div>
   </div>
 </template>
 
 <script>
+import Product from "../components/Seller/CreateProduct.vue";
+import Offer from "../components/Seller/CreateOffer.vue";
+
 export default {
-  components: {},
-  setup() {},
+  components: {
+    Product,
+    Offer,
+  },
+  data() {
+    return {
+      displayProduct: false,
+      displayOffer: false,
+    };
+  },
+  methods: {
+    openProduct() {
+      this.displayOffer = false;
+      this.displayProduct = true;
+    },
+    openOffer() {
+      this.displayOffer = true;
+      this.displayProduct = false;
+    },
+  },
 };
 </script>
