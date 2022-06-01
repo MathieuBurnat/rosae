5a4a13bf701c38fbe265bb9cde41e28121b0cee7
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2022-06-01 19:21:22 +0200

    create tracker activities

--- src/components/Product/ModalProductDetails.vue
+++ src/components/Product/ModalProductDetails.vue
@@ -2,15 +2,17 @@
   <div v-if="isOpen">
     <input type="checkbox" id="my-modal-5" class="modal-toggle" />
     <div class="modal modal-open" v-if="isOpen">
-      <div class="w-11/12 max-w-full modal-box">
+      <div class="w-11/12 max-w-full modal-box bg-zinc-100">
         <div class="text-center">
           <h1 class="text-lg font-bold">{{ product.name }}</h1>
           <div class="text-sm opacity-50">{{ product.id }}</div>
         </div>
 
-        <div class="m-8">
-          <div class="font-semibold">Information about the product</div>
-          <ul class="w-3/6 p-2 menu bg-base-100 rounded-box">
+        <div class="p-4 m-8 bg-zinc-50 rounded-2xl">
+          <div class="font-semibold text-center">
+            Information about the product
+          </div>
+          <ul class="w-full p-2 menu rounded-box">
             <li>
               <div>
                 <span class="w-full font-semibold">Status</span>
@@ -55,6 +57,21 @@
           </ul>
         </div>
 
+        <div class="p-4 m-8 bg-zinc-50 rounded-2xl">
+          <div class="mb-4 font-semibold text-center">Tracker activities</div>
+          <div class="w-full text-center">
+            <ul class="steps">
+              <li class="step">Created</li>
+              <li class="step">qrcode generated</li>
+              <li class="step">retrieved</li>
+              <li class="step step-accent">new owner</li>
+              <li class="step step-accent">Under Repair</li>
+              <li class="step step-accent">Ready</li>
+              <li class="step">Product Dead</li>
+            </ul>
+          </div>
+        </div>
+
         <div class="modal-action">
           <button class="btn btn-success" @click="isOpen = false">
             Go Back
@@ -74,14 +91,13 @@ import datePretty from "../../assets/dateFormater.js";
 export default {
   props: ["isOpen", "product"],
   components: {
-    FriendlyStatus
+    FriendlyStatus,
   },
   data() {
     return {
       date: datePretty(this.product.warrantyExpiresOn),
     };
   },
-  computed: {
-  },
+  computed: {},
 };
 </script>
