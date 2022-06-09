42e8bbdbe58aa5bf434ba830ebefddd2c2b99a01
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2022-06-09 13:10:42 +0200

    Create tab

--- src/components/Client/Retrieve.vue
+++ src/components/Client/Retrieve.vue
@@ -5,13 +5,36 @@
         <label class="block mb-2 text-sm font-bold text-gray-700" for="qrcode">
           qrcode
         </label>
-        <input
-          class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-          id="qrcode"
-          type="text"
-          placeholder="qrcode"
-          v-model="qrcode"
-        />
+        <div>
+          <div class="my-8 tabs">
+            <btn
+              @click="openScan"
+              class="w-2/4 tab tab-bordered"
+              :class="{ 'tab-active': showScanner }"
+              >Scan the QR Code</btn
+            >
+            <btn
+              @click="openMan"
+              class="w-2/4 tab tab-bordered"
+              :class="{ 'tab-active': showInput }"
+              >Write manually the QR Code</btn
+            >
+          </div>
+          <div>
+            <div v-if="showScanner">
+              <p>Scanner :]</p>
+            </div>
+            <div v-if="showInput">
+              <input
+                class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                id="qrcode"
+                type="text"
+                placeholder="qrcode"
+                v-model="qrcode"
+              />
+            </div>
+          </div>
+        </div>
       </div>
 
       <div class="mb-4">
@@ -55,6 +78,8 @@ export default {
       success: "",
       qrcode: "",
       ownerId: "",
+      showScanner: false,
+      showInput: false,
     };
   },
   components: {
@@ -74,7 +99,7 @@ export default {
       })
         .then((response) => {
           console.log(response);
-          
+
           this.success = true;
 
           this.message = {
@@ -86,6 +111,14 @@ export default {
           this.success = false;
           this.errors = this.$refs.errorManager.friendlyMessage(error);
         });
+    },
+    openScan() {
+      this.showScanner = true;
+      this.showInput = false;
+    },
+    openMan() {
+      this.showScanner = false;
+      this.showInput = true;
     },
   },
 };
