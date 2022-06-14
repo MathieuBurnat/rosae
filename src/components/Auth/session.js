963b418d853801cec2528307f639261159a6b601
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2022-06-14 09:38:02 +0200

    create resetKeypair()

--- src/components/Auth/session.js
+++ src/components/Auth/session.js
@@ -4,8 +4,9 @@ import { reactive } from 'vue'
 export const store = reactive({
     keypair: null,
     setKeypair(keypair) {
-        console.log("temp save");
         this.keypair = keypair
-        console.log(this.keypair);
+    },
+    resetKeypair() {
+        this.keypair = null
     },
 })
