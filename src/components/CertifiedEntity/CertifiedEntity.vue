450841c8edb4720da0a4c9805231f7f049eb53a0
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2022-06-15 14:01:07 +0200

    If the product is not certified by someone, add an warn-indicator

--- src/components/CertifiedEntity/CertifiedEntity.vue
+++ src/components/CertifiedEntity/CertifiedEntity.vue
@@ -44,8 +44,27 @@
     </div>
   </div>
   <div v-else>
-    {{ product.certifiedBy }}
-  </div>
+    <div class="indicator">
+      <span class="text-red-700 indicator-item">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="w-5 h-5"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10 1.944A11.954 11.954 0 012.166 5C2.056 5.649 2 6.319 2 7c0 5.225 3.34 9.67 8 11.317C14.66 16.67 18 12.225 18 7c0-.682-.057-1.35-.166-2.001A11.954 11.954 0 0110 1.944zM11 14a1 1 0 11-2 0 1 1 0 012 0zm0-7a1 1 0 10-2 0v3a1 1 0 102 0V7z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </span>
+      <div
+        class="grid w-32 h-10 border-2 border-dashed bg-base-150 border-color place-items-center"
+      >
+        Not Certified
+      </div>
+    </div>  </div>
 </template>
 
 <script>
